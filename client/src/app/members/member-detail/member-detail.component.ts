e7ccc720c8c33d0818a90a0c157c2cc18347dc41
Heinrich Barnard <heinrich.b1997@gmail.com>
2021-11-22 06:17:42 +0200

    Edit User Interface

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -26,7 +26,7 @@ export class MemberDetailComponent implements OnInit {
         imagePercent: 100,
         thumbnailsColumns: 4,
         imageAnimation: NgxGalleryAnimation.Slide,
-        preview: false
+        preview: true
 
       }
     ]
